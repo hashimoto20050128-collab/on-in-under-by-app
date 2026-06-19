b528b82b214704d198d8862d7f7fe3000d0f2860
hashimoto20050128-collab <hashimoto20050128@gmail.com>
2026-06-20 00:06:28 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,13 +5,13 @@
    Realtime Database を使うので databaseURL が必要です。
    ========================================================================= */
 const firebaseConfig = {
-  apiKey:            "ここにAPIキー",
-  authDomain:        "あなたのプロジェクト.firebaseapp.com",
-  databaseURL:       "https://あなたのプロジェクト-default-rtdb.firebasedatabase.app",
-  projectId:         "あなたのプロジェクト",
-  storageBucket:     "あなたのプロジェクト.appspot.com",
-  messagingSenderId: "000000000000",
-  appId:             "1:000000000000:web:xxxxxxxxxxxx"
+  apiKey: "AIzaSyAGQAXl9Mkj0jLXFQ9itu0tHrHaTkk3spE",
+  authDomain: "on-in-under-by.firebaseapp.com",
+  databaseURL: "https://on-in-under-by-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "on-in-under-by",
+  storageBucket: "on-in-under-by.firebasestorage.app",
+  messagingSenderId: "870802633980",
+  appId: "1:870802633980:web:c72f9b14bcb6fb2591d379"
 };
 
 /* ---- ここから下は変更不要 ---- */
